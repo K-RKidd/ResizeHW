dcabe03ce6d36216543f4bbbd0a4e63a3b9998ca
K-RKidd <kr248196@muhlenberg.edu>
2014-02-09 13:49:33 -0500

    Initial Commit

---
+++ ResizeHW/ResizeHWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ResizeHWAppDelegate.h
+//  ResizeHW
+//
+//  Created by Krystle on 2/9/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResizeHWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ResizeHW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ResizeHW
+//
+//  Created by Krystle on 2/9/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ResizeHWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ResizeHWAppDelegate class]));
+    }
+}
